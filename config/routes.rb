7cb5f633d2fa9d50d7de51893499b1227d10bb8b
hiroaki YAMADA <hiroakiteruyayamada@gmail.com>
2024-06-11 20:26:35 +0900

    20240611

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "show/id" => "show#lshow"
   get '/' => 'home#top'
   get 'about' => 'home#about'
-#７行目は仮
+  post "lposts/hako" => "lposts#hako"
+#７,10行目は仮
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
